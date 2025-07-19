cff50ce6221b9f17d849c0aff5a693ec2047187a
combo <kb144536@163.com>
2025-07-20 00:06:22 +0800

    Feat:添加cmake配置1

---
+++ build/CMakeFiles/SDLShooter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLShooter.dir/main.cpp.o"
+  "CMakeFiles/SDLShooter.dir/main.cpp.o.d"
+  "SDLShooter"
+  "SDLShooter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLShooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
